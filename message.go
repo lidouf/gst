dba24326b571b4a98b0a4d588ff6c60970f618a4
lidou <lidouf@gmail.com>
2016-07-27 20:48:49 +0800

    bug fix

--- message.go
+++ message.go
@@ -147,8 +147,8 @@ func (m *Message) ParseError() (err *glib.Error, debug string) {
 	var e, ret_e *C.GError
 
 	C.gst_message_parse_error(m.g(), &e, &d)
-	defer C.free(unsafe.Pointer(e))
-	defer C.free(unsafe.Pointer(d))
+	//defer C.free(unsafe.Pointer(e))
+	//defer C.free(unsafe.Pointer(d))
 
 	debug = C.GoString((*C.char)(d))
 	ret_e = new(C.GError)
