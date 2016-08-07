6d8f95dc636432d9d349f73b117bbd0dc36cf8b0
lidouf <lidouf@gmail.com>
2016-08-07 17:12:59 +0800

    add ClockTime definition & DurationOutput for ClockTime

--- clock.go
+++ clock.go
@@ -5,6 +5,9 @@ package gst
 static gboolean clock_time_is_valid(gint64 t) {
 	return GST_CLOCK_TIME_IS_VALID(t);
 }
+static gchar* clock_duration_output(GstClockTime t) {
+	return g_strdup_printf("%" GST_TIME_FORMAT, GST_TIME_ARGS(t));
+}
 */
 import "C"
 
@@ -18,6 +21,20 @@ func (c *Clock) g() *C.GstClock {
 
 func (c *Clock) AsClock() *Clock {
 	return c
+}
+
+type ClockTime C.GstClockTime
+
+func (t *ClockTime) g() *C.GstClockTime {
+	return (*C.GstClockTime)(t)
+}
+
+func (t ClockTime) AsUint64() uint64 {
+	return uint64(t)
+}
+
+func (t ClockTime) DurationOutput() string {
+	return C.GoString((*C.char)(C.clock_duration_output(C.GstClockTime(t))))
 }
 
 func ClockTimeIsValid(t int64) bool {
