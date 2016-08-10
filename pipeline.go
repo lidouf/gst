ef5eddba5fdd5395eaeb4ba6f1e886a39679b102
lidouf <lidouf@gmail.com>
2016-08-10 20:03:15 +0800

    pipeline implementation&doc

--- pipeline.go
+++ pipeline.go
@@ -23,6 +23,60 @@ func (p *Pipeline) AsPipeline() *Pipeline {
 	return p
 }
 
+func (p *Pipeline) GetBus() *Bus {
+	bus := C.gst_pipeline_get_bus(p.g())
+	if bus == nil {
+		return nil
+	}
+	b := new(Bus)
+	b.SetPtr(glib.Pointer(bus))
+	return b
+}
+
+func (p *Pipeline) SetClock(clock *Clock) bool {
+	return C.gst_pipeline_set_clock(p.g(), clock.g()) != 0
+}
+
+func (p *Pipeline) GetPipelineClock() *Clock {
+	return (*Clock)(unsafe.Pointer(C.gst_pipeline_get_pipeline_clock(p.g())))
+}
+
+func (p *Pipeline) GetClock() *Clock {
+	return (*Clock)(unsafe.Pointer(C.gst_pipeline_get_clock(p.g())))
+}
+
+func (p *Pipeline) UseClock(clock *Clock) {
+	C.gst_pipeline_use_clock(p.g(), clock.g())
+}
+
+func (p *Pipeline) AutoClock() {
+	C.gst_pipeline_auto_clock(p.g())
+}
+
+func (p *Pipeline) SetAutoFlushBus(autoFlush bool) {
+	C.gst_pipeline_set_auto_flush_bus(p.g(), autoFlush)
+}
+
+func (p *Pipeline) GetAutoFlushBus() bool {
+	return C.gst_pipeline_get_auto_flush_bus(p.g()) != 0
+}
+
+func (p *Pipeline) SetDelay(delay ClockTime) {
+	C.gst_pipeline_set_delay(p.g(), delay.AsUint64())
+}
+
+func (p *Pipeline) GetDelay() ClockTime {
+	return ClockTime(uint64(C.gst_pipeline_get_delay(p.g())))
+}
+
+func (p *Pipeline) SetLatency(latency ClockTime) {
+	C.gst_pipeline_set_latency(p.g(), latency.AsUint64())
+}
+
+func (p *Pipeline) GetLatency() ClockTime {
+	return ClockTime(uint64(C.gst_pipeline_get_delay(p.g())))
+}
+
 func NewPipeline(name string) *Pipeline {
 	s := (*C.gchar)(C.CString(name))
 	defer C.free(unsafe.Pointer(s))
---
+++ pipeline.md
@@ -0,0 +1,17 @@
+**progress**
+13/13
+```
+Y   GstElement * 	gst_pipeline_new ()
+Y   GstBus * 	gst_pipeline_get_bus ()
+Y   gboolean 	gst_pipeline_set_clock ()
+Y   GstClock * 	gst_pipeline_get_pipeline_clock ()
+Y   GstClock * 	gst_pipeline_get_clock ()
+Y   void 	gst_pipeline_use_clock ()
+Y   void 	gst_pipeline_auto_clock ()
+Y   void 	gst_pipeline_set_auto_flush_bus ()
+Y   gboolean 	gst_pipeline_get_auto_flush_bus ()
+Y   void 	gst_pipeline_set_delay ()
+Y   GstClockTime 	gst_pipeline_get_delay ()
+Y   void 	gst_pipeline_set_latency ()
+Y   GstClockTime 	gst_pipeline_get_latency ()
+```
