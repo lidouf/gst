d67fcd8d198628f68e1fc4518863b910bcd9aa45
lidouf <lidouf@gmail.com>
2016-08-10 19:39:59 +0800

    pad_template implementation & doc

--- pad_template.go
+++ pad_template.go
@@ -3,12 +3,14 @@ package gst
 /*
 #include <stdlib.h>
 #include <gst/gst.h>
+static gboolean pad_template_is_fixed(GstPadTemplate *templ) {
+	return GST_PAD_TEMPLATE_IS_FIXED(templ);
+}
 */
 import "C"
 
 import (
-//"github.com/lidouf/glib"
-//"unsafe"
+	"unsafe"
 )
 
 type PadTemplate C.GstPadTemplate
@@ -17,10 +19,32 @@ func (t *PadTemplate) g() *C.GstPadTemplate {
 	return (*C.GstPadTemplate)(t)
 }
 
+func (t *PadTemplate) GetCaps() *Caps {
+	return (*Caps)(unsafe.Pointer(C.gst_pad_template_get_caps(t.g())))
+}
+
+func (t *PadTemplate) IsFixed() bool {
+	return C.pad_template_is_fixed(t.g()) != 0
+}
+
+func NewPadTemplate(name string, direction PadDirection, presence PadPresence, caps *Caps) *PadTemplate {
+	s := (*C.gchar)(C.CString(name))
+	defer C.free(unsafe.Pointer(s))
+	return (*PadTemplate)(unsafe.Pointer(C.gst_pad_template_new(s, direction.g(), presence.g(), caps.g())))
+}
+
 type StaticPadTemplate C.GstStaticPadTemplate
 
 func (t *StaticPadTemplate) g() *C.GstStaticPadTemplate {
 	return (*C.GstStaticPadTemplate)(t)
+}
+
+func (t *StaticPadTemplate) Get() *PadTemplate {
+	return (*PadTemplate)(unsafe.Pointer(C.gst_static_pad_template_get(t.g())))
+}
+
+func (t *StaticPadTemplate) GetCaps() *Caps {
+	return (*Caps)(unsafe.Pointer(C.gst_static_pad_template_get_caps(t.g())))
 }
 
 func (t *StaticPadTemplate) GetNameTemplate() string {
---
+++ pad_template.md
@@ -0,0 +1,14 @@
+**progress**
+9/10
+```
+    #define 	GST_STATIC_PAD_TEMPLATE()
+Y   GstPadTemplate * 	gst_static_pad_template_get ()
+Y   GstCaps * 	gst_static_pad_template_get_caps ()
+Y   #define 	GST_PAD_TEMPLATE_NAME_TEMPLATE()
+Y   #define 	GST_PAD_TEMPLATE_DIRECTION()
+Y   #define 	GST_PAD_TEMPLATE_PRESENCE()
+Y   #define 	GST_PAD_TEMPLATE_CAPS()
+Y   #define 	GST_PAD_TEMPLATE_IS_FIXED()
+Y   GstPadTemplate * 	gst_pad_template_new ()
+Y   GstCaps * 	gst_pad_template_get_caps ()
+```
